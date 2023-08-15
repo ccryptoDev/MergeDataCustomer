f2dd814abc2c30d23f75b67c00568aa6023005b7
Mario Ayerve <ladtprojects@gmail.com>
2023-08-15 06:51:06 -0400

    MDC-167

---
+++ MergeDataCustomer/Application/Mappings/ReportProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MergeDataCustomer.Repositories.DtoModels.Responses;
+using MergeDataEntities.Schemas.Reports;
+
+namespace MergeDataCustomer.Application.Mappings
+{
+    public class ReportProfile : Profile
+    {
+        public ReportProfile()
+        {
+            CreateMap<Report, ReportConfigResponse>().ReverseMap();
+            CreateMap<Report, ReportContractResponse>().ReverseMap();
+            CreateMap<Report, ReportSummaryResponse>().ReverseMap();
+            CreateMap<ReportLine, ReportLineResponse>().ReverseMap();
+        }
+    }
+}
