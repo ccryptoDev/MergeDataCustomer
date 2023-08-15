3b06658041eef8050a38c49ae7e9d4395700d3df
ccryptoDev <smartwebdev90@gmail.com>
2023-08-15 07:02:28 -0400

    MDC-170

---
+++ MergeDataCustomer/Repositories/DtoModels/Requests/GetLineDrilldownRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace MergeDataCustomer.Repositories.DtoModels.Requests
+{
+    public class GetLineDrilldownRequest
+    {
+        public int Level { get; set; }
+        public long? ReportLineId { get; set; }
+        public string? AccountNo { get; set; }
+        public List<long>? StoreId { get; set; }
+        public List<string> Period { get; set; }
+    }
+}
