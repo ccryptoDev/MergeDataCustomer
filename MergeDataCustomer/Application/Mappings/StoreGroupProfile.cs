1ed81d287a531665ba6ff615b0b6872fa2da9dc2
ccryptoDev <smartwebdev90@gmail.com>
2023-08-15 07:03:40 -0400

    MDC-171

---
+++ MergeDataCustomer/Application/Mappings/StoreGroupProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MergeDataCustomer.Repositories.DtoModels.Responses;
+using MergeDataEntities.Schemas.Public;
+
+namespace MergeDataCustomer.Application.Mappings
+{
+    public class StoreGroupProfile : Profile
+    {
+        public StoreGroupProfile()
+        {
+            CreateMap<StoreGroup, StoreGroupResponse>().ReverseMap();
+        }
+    }
+}
