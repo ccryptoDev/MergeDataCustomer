098d407a3141222bddcbfdd08fcbb0a9f5d115f9
ccryptoDev <smartwebdev90@gmail.com>
2023-08-22 16:35:06 -0400

    MDC-182

---
+++ MergeDataCustomer/Application/Services/GeneralService.cs
@@ -0,0 +1,34 @@
+﻿using MergeDataCustomer.Repositories.DtoModels.Responses;
+using MergeDataImporter.Helpers.Generic;
+using MergeDataImporter.Repositories.Context;
+
+namespace MergeDataCustomer.Application.Services
+{
+    public class GeneralService
+    {
+        private readonly RawContext _context;
+
+        public GeneralService(RawContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Result<SystemDateResponse>> GetSystemDate(long clientId)
+        {
+            var clientData = _context.Clients.FirstOrDefault(x => x.ClientId == clientId && x.IsActive);
+            if (clientData == null)
+                return await Result<SystemDateResponse>.FailAsync("Client not found for given id");
+
+            SystemDateResponse result = new SystemDateResponse()
+            {
+                ClientId = clientId,
+                CurrentDate = DateTime.UtcNow,
+                YearsBackward = clientData.YearsBackward,
+                LastAccUpdate = DateTime.UtcNow,
+                LastFIUpdate = DateTime.UtcNow
+            };
+
+            return await Result<SystemDateResponse>.SuccessAsync(result);
+        }
+    }
+}
