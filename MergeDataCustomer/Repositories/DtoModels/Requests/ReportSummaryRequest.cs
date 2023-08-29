75db3472293dee07965bca6ace5f66b2a51a2bba
ccryptoDev <smartwebdev90@gmail.com>
2023-08-28 23:26:18 -0400

    MDC-74

---
+++ MergeDataCustomer/Repositories/DtoModels/Requests/ReportSummaryRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace MergeDataCustomer.Repositories.DtoModels.Requests
+{
+    public class ReportSummaryRequest
+    {
+        public long ReportId { get; set; }
+        public long ClientId { get; set; }
+    }
+}
