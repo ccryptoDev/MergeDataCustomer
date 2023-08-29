bc22372dc9ca56496c9f9da76abf319a28945527
ccryptoDev <smartwebdev90@gmail.com>
2023-08-28 23:26:27 -0400

    MDC-76

---
+++ MergeDataCustomer/Repositories/DtoModels/Responses/ReportConfigResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace MergeDataCustomer.Repositories.DtoModels.Responses
+{
+    public class ReportConfigResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Visible { get; set; }
+        public string Style { get; set; }
+
+        public long? ClientId { get; set; }
+
+        public List<string> Columns { get; set; }
+    }
+}
