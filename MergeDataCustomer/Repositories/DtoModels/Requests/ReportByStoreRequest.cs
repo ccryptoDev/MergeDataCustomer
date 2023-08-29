45c69f0d9ef4d53bbb3649516b463d6103c32067
ccryptoDev <smartwebdev90@gmail.com>
2023-08-28 23:26:03 -0400

    MDC-63

---
+++ MergeDataCustomer/Repositories/DtoModels/Requests/ReportByStoreRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace MergeDataCustomer.Repositories.DtoModels.Requests
+{
+    public class ReportByStoreRequest
+    {
+        public long ReportId { get; set; }
+        public long ClientId { get; set; }
+        public long StoreId { get; set; }
+        public List<string> Period { get; set; }
+    }
+}
