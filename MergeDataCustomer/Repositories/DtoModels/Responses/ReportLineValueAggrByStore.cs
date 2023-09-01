5797afa8d38dcd6963fdd842f97946a9733875a6
ccryptoDev <smartwebdev90@gmail.com>
2023-08-31 23:43:51 -0400

    MDC-78

---
+++ MergeDataCustomer/Repositories/DtoModels/Responses/ReportLineValueAggrByStore.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MergeDataCustomer.Repositories.DtoModels.Responses
+{
+    public class ReportLineValueAggrByStore
+    {
+        public long StoreId { get; set; }
+
+        public long ReportLineId { get; set; }
+        public string ColumnValue { get; set; }
+    }
+}
