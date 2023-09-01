9b5ac1c9dbea658257f7e4c496b0490242ea7291
ccryptoDev <smartwebdev90@gmail.com>
2023-08-31 23:42:42 -0400

    MDC-65

---
+++ MergeDataCustomer/Repositories/DtoModels/Responses/ReportDetailResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace MergeDataCustomer.Repositories.DtoModels.Responses
+{
+    public class ReportDetailResponse
+    {
+        public ReportConfigResponse ReportConfig { get; set; }
+        public List<ReportLineResponse> ReportLines { get; set; }
+    }
+}
