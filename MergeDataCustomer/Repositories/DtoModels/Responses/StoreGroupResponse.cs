2771c842393adc7dde50f5296fd68cceb5ab62d3
ccryptoDev <smartwebdev90@gmail.com>
2023-09-05 21:35:36 -0400

    MDC-145

---
+++ MergeDataCustomer/Repositories/DtoModels/Responses/StoreGroupResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MergeDataCustomer.Repositories.DtoModels.Responses
+{
+    public class StoreGroupResponse
+    {
+        public long StoreGroupId { get; set; }
+        public long ClientId { get; set; }
+
+        public string Name { get; set; } = null!;
+        public int StoreGroupLevel { get; set; }
+
+        public bool? IsActive { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string? ModifiedBy { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
