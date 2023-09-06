8cb4bc6cb1a8decefcbea132e30482890f71ec9e
ccryptoDev <smartwebdev90@gmail.com>
2023-09-05 21:35:24 -0400

    MDC-148

---
+++ MergeDataCustomer/Repositories/DtoModels/Responses/StoreGroupItemResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace MergeDataCustomer.Repositories.DtoModels.Responses
+{
+    public class StoreGroupItemResponse
+    {
+        public long StoreGroupId { get; set; }
+        public long StoreId { get; set; }
+    }
+}
