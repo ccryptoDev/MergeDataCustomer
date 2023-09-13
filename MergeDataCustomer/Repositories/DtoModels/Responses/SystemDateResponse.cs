872331b2158215ee1aa1417516dfde1f7ec3c0dd
ccryptoDev <smartwebdev90@gmail.com>
2023-09-13 02:49:48 -0400

    MDC-149

---
+++ MergeDataCustomer/Repositories/DtoModels/Responses/SystemDateResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace MergeDataCustomer.Repositories.DtoModels.Responses
+{
+    public class SystemDateResponse
+    {
+        public long ClientId { get; set; }
+        public DateTime CurrentDate { get; set; }
+        public int YearsBackward { get; set; }
+
+        public DateTime LastAccUpdate { get; set; }
+        public DateTime LastFIUpdate { get; set; }
+    }
+}
