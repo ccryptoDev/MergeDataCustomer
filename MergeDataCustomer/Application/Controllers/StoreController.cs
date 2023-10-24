fb12f34a28dfb28c0d5822f64510498712f7046d
ccryptoDev <smartwebdev90@gmail.com>
2023-10-24 03:44:57 -0400

    MDC-207

--- MergeDataCustomer/Application/Controllers/StoreController.cs
+++ MergeDataCustomer/Application/Controllers/StoreController.cs
@@ -1,6 +1,7 @@
 ﻿using MergeDataCustomer.Application.Services;
 using MergeDataCustomer.Helpers.Configuration;
 using MergeDataCustomer.Repositories.DtoModels.Responses;
+using MergeDataCustomer.Repositories.DtoModels.Requests;
 using MergeDataImporter.Helpers.Generic;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -31,5 +32,39 @@ namespace MergeDataCustomer.Application.Controllers
             return Ok(result);
         }
 
+        [Route("GetStoreGroups")]
+        [Authorize(Policy = Permissions.User.View)]
+        [HttpGet]
+        public async Task<IActionResult> GetStoreGroups(long clientId)
+        {
+            var auxResult = await _storeService.GetStoreGroups(clientId);
+            List<StoreGroupResponse> result = auxResult.Data;
+
+            return Ok(result);
+        }
+
+        [Route("CreateStoreGroup")]
+        [Authorize(Policy = Permissions.User.Create)]
+        [HttpPost]
+        public async Task<IActionResult> CreateStoreGroup([FromBody] StoreGroupCreationRequest newStoreGroup)
+        {
+            var auxResult = await _storeService.CreateStoreGroup(newStoreGroup);
+            StoreGroupResponse result = auxResult.Data;
+
+            return CreatedAtAction(nameof(GetStoreGroups), new { id = result.StoreGroupId }, result);
+        }
+
+        [Route("DeleteStoreGroup/{id}")]
+        [Authorize(Policy = Permissions.User.Delete)]
+        [HttpDelete]
+        public async Task<IActionResult> DeleteStoreGroup(long id)
+        {
+            var result = await _storeService.DeleteStoreGroup(id);
+
+            if (result.Succeeded)
+                return NoContent();
+            else
+                return NotFound();
+        }
     }
 }
