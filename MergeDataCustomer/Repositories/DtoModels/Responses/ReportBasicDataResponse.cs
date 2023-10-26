ee6a194030732f5814ba9a7204e8a22c8dd0312b
ccryptoDev <smartwebdev90@gmail.com>
2023-10-26 09:42:29 -0400

    MDC-218

---
+++ MergeDataCustomer/Repositories/DtoModels/Responses/ReportBasicDataResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace MergeDataCustomer.Repositories.DtoModels.Responses
+{
+    public class ReportBasicDataResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Visible { get; set; }
+        public string Style { get; set; }
+        public string SummaryStyle { get; set; }
+
+        public string? CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
