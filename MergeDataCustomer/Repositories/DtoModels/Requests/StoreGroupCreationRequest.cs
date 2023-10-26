fea26ce000162af63283591f71be57a036ebafa3
ccryptoDev <smartwebdev90@gmail.com>
2023-10-26 09:42:18 -0400

    MDC-233

---
+++ MergeDataCustomer/Repositories/DtoModels/Requests/StoreGroupCreationRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using MergeDataEntities.Enums;
+
+namespace MergeDataCustomer.Repositories.DtoModels.Requests
+{
+    public class StoreGroupCreationRequest
+    {
+        public StoreGroupLevel StoreGroupLevel { get; set; }
+        public long ClientId { get; set; }
+        public string Name { get; set; }
+        public List<int> StoreIds { get; set; }
+    }
+}
