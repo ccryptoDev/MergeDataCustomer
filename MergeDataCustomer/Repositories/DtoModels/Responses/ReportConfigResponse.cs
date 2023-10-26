010b2d90076b9300b2938d195b0b68ef9ab38a20
ccryptoDev <smartwebdev90@gmail.com>
2023-10-26 09:42:38 -0400

    MDC-196

--- MergeDataCustomer/Repositories/DtoModels/Responses/ReportConfigResponse.cs
+++ MergeDataCustomer/Repositories/DtoModels/Responses/ReportConfigResponse.cs
@@ -1,4 +1,6 @@
-﻿namespace MergeDataCustomer.Repositories.DtoModels.Responses
+﻿using MergeDataEntities.Enums;
+
+namespace MergeDataCustomer.Repositories.DtoModels.Responses
 {
     public class ReportConfigResponse
     {
@@ -7,8 +9,24 @@
         public string Description { get; set; }
         public bool Visible { get; set; }
         public string Style { get; set; }
+        public string SummaryStyle { get; set; }
+        public ReportType ReportType { get; set; }
 
         public long? ClientId { get; set; }
+
+
+        public bool ByTrendEnabled { get; set; }
+        public bool ByStoreEnabled { get; set; }
+        public bool StoreSelectorEnabled { get; set; }
+
+        public bool DaySelectorEnabled { get; set; }
+        public bool MonthSelectorEnabled { get; set; }
+        public bool YearSelectorEnabled { get; set; }
+
+        public bool MtdYtdEnabled { get; set; }
+        public bool PriorMonthEnabled { get; set; }
+        public bool DrilldownEnabled { get; set; }
+
 
         public List<string> Columns { get; set; }
     }
