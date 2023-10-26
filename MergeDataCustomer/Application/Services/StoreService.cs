9397678a00dd7219f4f51a46047749c59c6d25b8
ccryptoDev <smartwebdev90@gmail.com>
2023-10-25 20:04:29 -0400

    MDC-216

--- MergeDataCustomer/Application/Services/StoreService.cs
+++ MergeDataCustomer/Application/Services/StoreService.cs
@@ -1,8 +1,11 @@
 ﻿using AutoMapper;
 using MergeDataCustomer.Repositories.DtoModels.Responses;
+using MergeDataCustomer.Repositories.DtoModels.Requests;
 using MergeDataEntities.Schemas.Public;
 using MergeDataImporter.Helpers.Generic;
 using MergeDataImporter.Repositories.Context;
+using Microsoft.EntityFrameworkCore;
+using MergeDataEntities.Enums;
 
 namespace MergeDataCustomer.Application.Services
 {
@@ -27,6 +30,68 @@ namespace MergeDataCustomer.Application.Services
             return await Result<List<StoreResponse>>.SuccessAsync(result);
         }
 
+        public async Task<Result<List<StoreGroupResponse>>> GetStoreGroups(long clientId)
+        {            
+            List<StoreGroup> storeGroups = _context.StoreGroups
+                .Where(x => x.ClientId == clientId && x.IsActive)
+                .Include(sg => sg.StoreGroupItems)
+                .ToList();
 
+            var result = _mapper.Map<List<StoreGroupResponse>>(storeGroups);
+
+            return await Result<List<StoreGroupResponse>>.SuccessAsync(result);
+        }
+
+        public async Task<Result<StoreGroupResponse>> CreateStoreGroup(StoreGroupCreationRequest request)
+        {
+            var storeGroup = new StoreGroup
+            {
+                ClientId = request.ClientId,
+                Name = request.Name,
+                IsActive = true,
+                StoreGroupLevel = (StoreGroupLevel)request.StoreGroupLevel
+            };
+
+            await _context.StoreGroups.AddAsync(storeGroup);
+
+            await _context.SaveChangesAsync();
+
+            foreach (var storeIdInt in request.StoreIds)
+            {
+                var storeGroupItem = new StoreGroupItem
+                {
+                    StoreId = storeIdInt,
+                    StoreGroupId = storeGroup.Id
+                };
+                await _context.StoreGroupItems.AddAsync(storeGroupItem);
+            }
+
+            await _context.SaveChangesAsync();
+
+            var result = _mapper.Map<StoreGroupResponse>(storeGroup);
+            result.StoreGroupItems = _context.StoreGroupItems
+                                             .Where(sgi => sgi.StoreGroupId == storeGroup.Id)
+                                             .Select(sgi => new StoreGroupItemResponse {
+                                                 StoreGroupId = sgi.StoreGroupId,
+                                                 StoreId = sgi.StoreId
+                                             })
+                                             .ToList();
+
+            return await Result<StoreGroupResponse>.SuccessAsync(result);
+        }
+
+        public async Task<Result<bool>> DeleteStoreGroup(long storeGroupId)
+        {
+            var storeGroup = await _context.StoreGroups.FirstOrDefaultAsync(x => x.Id == storeGroupId);
+
+            if (storeGroup == null)
+                return await Result<bool>.FailAsync("Store Group not found");
+
+            storeGroup.IsActive = false;
+           
+            await _context.SaveChangesAsync();
+
+            return await Result<bool>.SuccessAsync(true);
+        }
     }
 }
