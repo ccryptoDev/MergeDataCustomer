e76bbc104726508d3356ebf91e0f8cfccac0c0b2
ccryptoDev <smartwebdev90@gmail.com>
2024-01-31 00:02:20 -0400

    MDC-233

--- MergeDataCustomer/Application/Controllers/AccountController.cs
+++ MergeDataCustomer/Application/Controllers/AccountController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace MergeDataCustomer.Application.Controllers
 {
     [ApiController]
-    [ApiExplorerSettings(GroupName = "Layer4")]
+    [ApiExplorerSettings(GroupName = "Layer3")]
     [ApiVersion(ApiVersioning.CurrentVersion)]
     [Route("api/identity/[controller]")]
     public class AccountController : ControllerBase
