3409daa8536e4b9439cdee4db0133de510f0c98e
ccryptoDev <smartwebdev90@gmail.com>
2024-02-01 23:47:54 -0400

    MDC-153

--- MergeDataCustomer/Application/Controllers/ReportController.cs
+++ MergeDataCustomer/Application/Controllers/ReportController.cs
@@ -1,4 +1,4 @@
-﻿using AutoMapper;
+﻿using CsvHelper;
 using MergeDataCustomer.Application.Services;
 using MergeDataCustomer.Helpers.Configuration;
 using MergeDataCustomer.Repositories.DtoModels.Requests;
@@ -6,24 +6,29 @@ using MergeDataCustomer.Repositories.DtoModels.Responses;
 using MergeDataEntities.Schemas.Public;
 using MergeDataEntities.Schemas.Reports;
 using MergeDataImporter.Helpers.Generic;
+using MergeDataImporter.Repositories.Context;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Org.BouncyCastle.Asn1.Ocsp;
+using System.Globalization;
+using System.Reflection;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace MergeDataCustomer.Application.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    [ApiExplorerSettings(GroupName = "Layer4")]
+    [ApiExplorerSettings(GroupName = "Layer3")]
     [ApiVersion(ApiVersioning.CurrentVersion)]
     public class ReportController : ControllerBase
     {
         private readonly ReportService _reportService;
+        private readonly RawContext _context;
 
-        public ReportController(ReportService reportService)
+        public ReportController(ReportService reportService, RawContext context)
         {
             _reportService = reportService;
+            _context = context;
         }
 
         [Route("GetSections")]
@@ -50,13 +55,119 @@ namespace MergeDataCustomer.Application.Controllers
 
         [Route("GetReportListBySubSection")]
         [Authorize(Policy = Permissions.User.View)]
-        [HttpGet]
-        public async Task<IActionResult> GetReportListBySubSection(long subSectionId, long clientId, string userId)
+        [HttpPost]
+        public async Task<IActionResult> GetReportListBySubSection(GetReportListBySubSectionRequest request)
         {
-            var result = await _reportService.GetReportListBySubSection(subSectionId, clientId, userId);
+            var result = await _reportService.GetReportListBySubSection(request.SubSectionId, request.ClientId, request.StoreId.First(), request.UserId, request.Period, request.Target);
 
             return Ok(result);
         }
+
+        [Route("GetUpdatedReportSummary")]
+        [Authorize(Policy = Permissions.User.View)]
+        [HttpPost]
+        public async Task<IActionResult> GetUpdatedReportSummary(GetReportSummaryRequest request)
+        {
+            var result = await _reportService.GetUpdatedReportSummary(request.ReportId, request.ClientId, request.StoreId.First(), request.Period, request.Target, request.SelectedOptions);
+
+            return Ok(result);
+        }
+
+        [Route("GetCSVReport")]
+        [Authorize(Policy = Permissions.User.View)]
+        [HttpPost]
+        public async Task<IActionResult> GetCSVReport(GetReportRequest request)
+        {
+            var reportResult = await GetReport(request) as OkObjectResult;
+
+            if (reportResult == null || !(reportResult.Value is ReportDetailResponse))
+            {
+                return BadRequest("Failed to generate report");
+            }
+
+            var reportData = reportResult.Value as ReportDetailResponse;
+
+            var csvBuilder = new StringBuilder();
+            var stringWriter = new StringWriter(csvBuilder);
+
+            using (var csvWriter = new CsvWriter(stringWriter, CultureInfo.InvariantCulture))
+            {
+                if(reportData.ReportConfig.Columns.Count > 0 && reportData.ReportLines.Count > 0)
+                {
+                    foreach (var header in reportData.ReportConfig.Columns) //headers
+                    {
+                        csvWriter.WriteField(header);
+                    }
+                    csvWriter.NextRecord();
+
+                    foreach (var record in reportData.ReportLines) //records
+                    {
+                        foreach (var field in record.Values) //fields
+                        {
+                            csvWriter.WriteField(field.TrimEnd());
+                        }   
+                        csvWriter.NextRecord();
+                    }
+                }
+                else //PATCH for demo: special case of getting data directly from the normalized tables
+                {
+                    //first and last day of period 
+                    var period = request.Period.First().Split("-");
+                    DateOnly firstDay = new DateOnly(Convert.ToInt32(period[0]), Convert.ToInt32(period[1]), 1);
+                    DateOnly lastDay = new DateOnly(Convert.ToInt32(period[0]), Convert.ToInt32(period[1]), DateTime.DaysInMonth(Convert.ToInt32(period[0]), Convert.ToInt32(period[1])));
+
+                    IEnumerable<Object> normalizedData = new List<Object>();
+
+                    switch (reportData.ReportConfig.Name)
+                    {
+                        case string a when a.Contains("Service"):
+                            normalizedData = _context.NormalizedServices.Where(x => x.ClientId == request.ClientId &&
+                                                                                     request.StoreId.Contains(x.StoreId) && 
+                                                                                     x.DateClosed >= firstDay && x.DateClosed <= lastDay)
+                                                                        .ToList();
+                            break;
+                        case string a when a.Contains("Sale"):
+                            normalizedData = _context.NormalizedSales.Where(x => x.ClientId == request.ClientId &&
+                                                                                     request.StoreId.Contains(x.StoreId) &&
+                                                                                     x.DealDate >= firstDay && x.DealDate <= lastDay)
+                                                                      .ToList();
+
+                            break;
+                    }
+
+                    if (normalizedData.ToList().Count > 0)
+                    {
+                        foreach (PropertyInfo prop in normalizedData.First().GetType().GetProperties())
+                        {
+                            if (prop.Name != "IsActive" && prop.Name != "CreatedBy" && prop.Name != "CreatedOn" && prop.Name != "ModifiedBy" && prop.Name != "ModifiedOn" && prop.Name != "Id" && prop.Name != "ClientId" && prop.Name != "StoreId" && prop.Name != "Client" && prop.Name != "Store" && //generic fields
+                                prop.Name != "FirstName" && prop.Name != "MiddleName" && prop.Name != "Lastname" && prop.Name != "Birthdate" && prop.Name != "Address1" && prop.Name != "Address2" && prop.Name != "City" && prop.Name != "State" && prop.Name != "WorkPhone" && prop.Name != "Phone" && prop.Name != "WorkPhoneExt" && //service fields
+                                prop.Name != "CustomerId" && prop.Name != "LastName" && prop.Name != "BirthDate" && prop.Name != "Address" && prop.Name != "AddressInfo" && prop.Name != "Email") //sales fields
+                                csvWriter.WriteField(prop.Name);
+                        }
+                        csvWriter.NextRecord();
+
+                        foreach (var record in normalizedData) //records
+                        {
+                            foreach (PropertyInfo prop in record.GetType().GetProperties())
+                            {
+                                //avoid adding nonsense fields
+                                if (prop.Name != "IsActive" && prop.Name != "CreatedBy" && prop.Name != "CreatedOn" && prop.Name != "ModifiedBy" && prop.Name != "ModifiedOn" && prop.Name != "Id" && prop.Name != "ClientId" && prop.Name != "StoreId" && prop.Name != "Client" && prop.Name != "Store" && //generic fields
+                                prop.Name != "FirstName" && prop.Name != "MiddleName" && prop.Name != "Lastname" && prop.Name != "Birthdate" && prop.Name != "Address1" && prop.Name != "Address2" && prop.Name != "City" && prop.Name != "State" && prop.Name != "WorkPhone" && prop.Name != "Phone" && prop.Name != "WorkPhoneExt" && //service fields
+                                prop.Name != "CustomerId" && prop.Name != "LastName" && prop.Name != "BirthDate" && prop.Name != "Address" && prop.Name != "AddressInfo" && prop.Name != "Email") //sales fields
+                                {
+                                    var celldata = record.GetType().GetProperty(prop.Name)?.GetValue(record)?.ToString()?.Trim();
+                                    csvWriter.WriteField(celldata);
+                                }
+                            }
+                            csvWriter.NextRecord();
+                        }
+                    }
+                }
+            }
+
+            return File(new MemoryStream(Encoding.UTF8.GetBytes(csvBuilder.ToString())), "text/csv", "Report.csv");
+        }
+
 
         [Route("GetReport")]
         [Authorize(Policy = Permissions.User.View)]
@@ -154,15 +265,15 @@ namespace MergeDataCustomer.Application.Controllers
                 return BadRequest(result.Messages);
         }
 
-        [Route("GetReportSummaries")]
-        [Authorize(Policy = Permissions.User.View)]
-        [HttpPost]
-        public async Task<IActionResult> GetReportSummaries(int subSectionID)
-        {
-            var result = await _reportService.GetReportSummaries(subSectionID);
+        //[Route("GetReportSummaries")]
+        //[Authorize(Policy = Permissions.User.View)]
+        //[HttpPost]
+        //public async Task<IActionResult> GetReportSummaries(int subSectionID)
+        //{
+        //    var result = await _reportService.GetReportSummaries(subSectionID);
 
-            return Ok(result);
-        }
+        //    return Ok(result);
+        //}
 
         [Route("GetLineDrilldown")]
         [Authorize(Policy = Permissions.User.View)]
