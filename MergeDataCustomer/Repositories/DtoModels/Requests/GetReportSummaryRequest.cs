848174c24a53e89dcb181fad0c071d14359d5b71
ccryptoDev <smartwebdev90@gmail.com>
2024-02-13 14:11:20 -0400

    MDC-27

---
+++ MergeDataCustomer/Repositories/DtoModels/Requests/GetReportSummaryRequest.cs
@@ -0,0 +1,16 @@
+﻿using MergeDataEntities.Enums;
+
+namespace MergeDataCustomer.Repositories.DtoModels.Requests
+{
+    public class GetReportSummaryRequest
+    {
+        public long ReportId { get; set; }
+        public long ClientId { get; set; }
+        public List<long>? StoreId { get; set; }
+        public TargetOption Target { get; set; }
+        public List<string> Period { get; set; }
+
+        //if the summary has selects with different options, here should come the selected options by index, separated by comma
+        public List<int>? SelectedOptions { get; set; }
+    }
+}
