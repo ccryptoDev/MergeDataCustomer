3820b28ef975d271f5ec159ff86ad0d4038aa5c7
ccryptoDev <smartwebdev90@gmail.com>
2024-02-13 14:11:12 -0400

    MDC-158

--- MergeDataCustomer/Repositories/DtoModels/Requests/GetReportRequest.cs
+++ MergeDataCustomer/Repositories/DtoModels/Requests/GetReportRequest.cs
@@ -1,8 +1,11 @@
-﻿namespace MergeDataCustomer.Repositories.DtoModels.Requests
+﻿using MergeDataEntities.Enums;
+
+namespace MergeDataCustomer.Repositories.DtoModels.Requests
 {
     public class GetReportRequest
     {
         public long ReportId { get; set; }
+        public TargetOption Target { get; set; }
         public long ClientId { get; set; }
         public List<long>? StoreId { get; set; }
         public List<string> Period { get; set; }
