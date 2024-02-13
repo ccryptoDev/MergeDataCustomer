17231fa48489a4d53578d651c7bf652569a66ae0
ccryptoDev <smartwebdev90@gmail.com>
2024-02-13 14:11:01 -0400

    MDC-152

---
+++ MergeDataCustomer/Repositories/DtoModels/Requests/GetReportListBySubSectionRequest.cs
@@ -0,0 +1,14 @@
+﻿using MergeDataEntities.Enums;
+
+namespace MergeDataCustomer.Repositories.DtoModels.Requests
+{
+    public class GetReportListBySubSectionRequest
+    {
+        public long SubSectionId { get; set; }
+        public long ClientId { get; set; }
+        public List<long>? StoreId { get; set; }
+        public string UserId { get; set; }
+        public TargetOption Target { get; set; }
+        public List<string> Period { get; set; }
+    }
+}
