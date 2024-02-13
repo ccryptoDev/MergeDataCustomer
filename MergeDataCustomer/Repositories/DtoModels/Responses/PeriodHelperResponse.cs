f30cafe2e636196c555eab8079a5d2b27996c9f1
ccryptoDev <smartwebdev90@gmail.com>
2024-02-13 14:11:34 -0400

    MDC-169

---
+++ MergeDataCustomer/Repositories/DtoModels/Responses/PeriodHelperResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace MergeDataCustomer.Repositories.DtoModels.Responses
+{
+    public class PeriodHelperResponse
+    {
+        public string[] PeriodParts { get; set; }
+        public int PriorMonth { get; set; }
+        public int ZeroToSubtract { get; set; }
+    }
+}
