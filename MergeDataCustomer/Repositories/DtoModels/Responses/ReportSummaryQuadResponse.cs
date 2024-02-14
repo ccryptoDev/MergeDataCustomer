5ec50e488789934cd4bbbb5d90f666c2d9a6a098
ccryptoDev <smartwebdev90@gmail.com>
2024-02-14 06:23:24 -0400

    MDC-48

---
+++ MergeDataCustomer/Repositories/DtoModels/Responses/ReportSummaryQuadResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace MergeDataCustomer.Repositories.DtoModels.Responses
+{
+    public class ReportSummaryQuadResponse
+    {
+        public string Title { get; set; }
+        public int Count { get; set; }
+    }
+}
