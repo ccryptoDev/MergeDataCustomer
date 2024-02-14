3de319716fd7103c2a10ecb9116010291cb1c0e7
ccryptoDev <smartwebdev90@gmail.com>
2024-02-14 06:23:16 -0400

    MDC-43

---
+++ MergeDataCustomer/Repositories/DtoModels/Responses/ReportSummaryBubbleResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace MergeDataCustomer.Repositories.DtoModels.Responses
+{
+    public class ReportSummaryBubbleResponse
+    {
+        public string LowerLimit { get; set; }
+        public string UpperLimit { get; set; }
+        public int Count { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
