fdac63b3cbcf758e7a8ac9c6d4bf35fc8b5d99fb
ccryptoDev <smartwebdev90@gmail.com>
2024-08-15 11:30:03 -0400

    MDC-162

---
+++ MergeDataCustomer/Repositories/DtoModels/Requests/StoreCreationRequest.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MergeDataCustomer.Repositories.DtoModels.Requests
+{
+    public class StoreCreationRequest
+    {
+        public long ClientId { get; set; }
+
+        public string Name { get; set; } = null!;
+        public string ShortName { get; set; }
+        public string AbbrName { get; set; }
+
+        public string Address { get; set; } = null!;
+        public string Zip { get; set; } = null!;
+
+
+        public long? DmsId { get; set; } = null;
+        public long? CmsId { get; set; } = null;
+        public long? CrmId { get; set; } = null;
+        public long? ErpId { get; set; } = null;
+
+
+        public DateTime StartDate { get; set; }
+    }
+}
