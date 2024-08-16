1576cd84a7e79b60dd92415fb2bc920a6e714ee4
ccryptoDev <smartwebdev90@gmail.com>
2024-08-16 08:20:03 -0400

    MEC-154

---
+++ MergeDataCustomer/Repositories/DtoModels/Responses/AgGrid/AgGridObject.cs
@@ -0,0 +1,8 @@
+﻿namespace MergeDataCustomer.Repositories.DtoModels.Responses.AgGrid
+{
+    public class AgGridObject
+    {
+        public List<AgGridColumn> Columns { get; set; }
+        public List<dynamic> ReportLines { get; set; }
+    }
+}
