58e8fac6c803edad410c556e231619fd097740d9
ccryptoDev <smartwebdev90@gmail.com>
2024-08-16 08:19:42 -0400

    MDC-238

---
+++ MergeDataCustomer/Repositories/DtoModels/Responses/ClientResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace MergeDataCustomer.Repositories.DtoModels.Responses
+{
+    public class ClientResponse
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+        public bool IsActive { get; set; }
+        public int StoresCount { get; set; }
+    }
+}
