054127532a40537fccaf361b12c0db653a35bdfd
ccryptoDev <smartwebdev90@gmail.com>
2024-08-16 08:19:29 -0400

    MDC-207

---
+++ MergeDataCustomer/Repositories/DtoModels/Requests/StoreUpdateRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace MergeDataCustomer.Repositories.DtoModels.Requests
+{
+    public class StoreUpdateRequest
+    {
+        public string Name { get; set; } = null!;
+        public string ShortName { get; set; }
+        public string AbbrName { get; set; }
+
+        public string Address { get; set; } = null!;
+        public string Zip { get; set; } = null!;
+
+
+        public long? DmsId { get; set; } = null;
+        public long? CmsId { get; set; } = null;
+        public long? CrmId { get; set; } = null;
+        public long? ErpId { get; set; } = null;
+    }
+}
